373da38d38d69bb7345ee8601910cae39cbda6ab
Pranavbc <pranav1952000@gmail.com>
2025-01-25 10:53:43 +0530

    api

--- assets/js/api.js
+++ assets/js/api.js
@@ -24,7 +24,9 @@
   
   export const url = {
     currentWeather(lat, lon){
-    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    // return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`;
+
     },
     forecast(lat, lon){
       return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
