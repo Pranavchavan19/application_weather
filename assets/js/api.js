81cb23076cabdbcb2a43d06404501813fff0ae3c
Pranavbc <pranav1952000@gmail.com>
2025-01-25 10:28:30 +0530

    https

--- assets/js/api.js
+++ assets/js/api.js
@@ -30,10 +30,10 @@
       return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
     },
     airPollution(lat, lon){
-      return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+      return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
     },
     reverseGeo(lat, lon){
-      return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+      return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
     },
     /**
      * 
@@ -41,7 +41,7 @@
      * @returns 
      */
     geo(query){
-      return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+      return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
     }
   }
 
